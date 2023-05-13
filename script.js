71a8766eb66b50ef3e42120224f701799ed83de9
Sarc4sticNerd <jplam904@gmail.com>
2023-05-12 22:02:58 -0700

    Finished Carousel and footer

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 $(function () {
   float();
+  $('.planet').hover(
+    function () {
+      this.src =
+        'images/Planet' +
+        String(Number(this.getAttribute('data-planet')) + 1) +
+        '.png';
+      $('html').css('cursor', 'pointer');
+      $('.pic').removeClass('d-none');
+      $('.pic').addClass('d-block');
+    },
+    function () {
+      this.src =
+        'images/Planet' +
+        String(Number(this.getAttribute('data-planet'))) +
+        '.png';
+      $('html').css('cursor', 'default');
+      $('.pic').removeClass('d-block');
+      $('.pic').addClass('d-none');
+    }
+  );
+  $('.planet').click(function () {
+    window.location.href = this.getAttribute('data-link');
+  });
+  $('#co').click(function () {
+    setTimeout(function () {
+      $('#contact').get(0).scrollIntoView({ behavior: 'smooth' });
+    }, 500);
+  });
 });
 
 function float() {
